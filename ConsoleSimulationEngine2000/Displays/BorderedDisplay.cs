f91350af190324fdf86bdba7caf763d87ffbf3bd
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-10-25 09:28:55 +0200

    meh

--- ConsoleSimulationEngine2000/Displays/BorderedDisplay.cs
+++ ConsoleSimulationEngine2000/Displays/BorderedDisplay.cs
@@ -10,6 +10,8 @@ namespace ConsoleSimulationEngine2000
         private static string regexColorStart = @"(" + '\u001b' + @"\[\d\d;2;\d{1,3};\d{1,3};\d{1,3}m)";
         private static string regexColorEnd = @"(" + '\u001b' + @"\[0m)";
         private static Regex colorRegex = new Regex(regexColorStart + "|" + regexColorEnd, RegexOptions.Compiled);
+        private string[] lines = new string[0];
+
         /// <summary>
         /// Creates a display to be shown at position (x, y) with the given width and height. 
         /// </summary>
@@ -26,7 +28,7 @@ namespace ConsoleSimulationEngine2000
         {
             set
             {
-                if (value.Contains(Environment.NewLine))
+                if (value.Contains(Environment.NewLine) || value.Contains('\n') || value.Contains('\r'))
                 {
                     throw new InvalidOperationException("Value can't contain newlines. Use the Lines property instead to set lines individually.");
                 }
@@ -36,14 +38,24 @@ namespace ConsoleSimulationEngine2000
                 }
             }
         }
-        public string[] Lines { get; set; } = new string[0];
-
+        public string[] Lines
+        {
+            get => lines; 
+            set
+            {
+                if(value.Any(v => v.Contains(Environment.NewLine) || v.Contains('\n') || v.Contains('\r')))
+                {
+                    throw new InvalidOperationException("Lines can't contain newlines. Add one line at a time to the Lines property.");
+                }
+                lines = value;
+            }
+        }
         protected internal override string GetStringToDisplay()
         {
-            
+
             var sb = new StringBuilder();
             sb.AppendLine("#" + "-".PadRight(GetWidth() - 2, '-') + "#");
-            
+
             for (int i = 0; i < GetHeight() - 2; i++)
             {
                 if (Lines.Count() > i)
